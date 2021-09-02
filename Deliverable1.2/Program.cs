fba472c209c1abb8cc8a6ba29f9a4fe17ad89b63
kalindih1 <73810475+kalindih1@users.noreply.github.com>
2021-09-02 18:06:38 -0400

    Completed Deliverable 1

---
+++ Deliverable1.2/Program.cs
@@ -0,0 +1,94 @@
+﻿
+using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string measurementType;
+        float amount = 0;
+        string response = "yes";
+
+
+       
+
+
+
+
+
+        while (response == "yes")
+        {
+
+            Console.WriteLine("Enter the measurement type:");
+            measurementType = Console.ReadLine();
+            Console.WriteLine("Enter an amount;");
+            amount = float.Parse(Console.ReadLine());
+
+
+
+
+            if (measurementType == "inches")
+            {
+
+                float inches, conversion, fidgetSpinners;
+                conversion = 3.5f;
+                inches = amount;
+                fidgetSpinners = (amount * conversion);
+                Console.WriteLine(fidgetSpinners + " fidget spinners ");
+
+
+                Console.WriteLine("Would you like to do another conversion? ");
+                response = Console.ReadLine();
+            }
+            else if (measurementType == "fidget spinners")
+            {
+
+                float inches, conversion, fidgetSpinners;
+                conversion = 3.5f;
+                fidgetSpinners = amount;
+                inches = amount / conversion;
+                Console.WriteLine(inches + " inches ");
+
+
+                Console.WriteLine("Would you like to do another conversion? ");
+                response = Console.ReadLine();
+            }
+            else if (measurementType == "feet")
+            {
+
+                float feet, conversion, memes;
+                conversion = 5f;
+                feet = amount;
+                memes = amount * conversion;
+                Console.WriteLine(memes + " memes ");
+
+
+                Console.WriteLine("Would you like to do another conversion? ");
+                response = Console.ReadLine();
+
+            }
+            else if (measurementType == "memes")
+            {
+                float feet, conversion, memes;
+                conversion = 5f;
+                memes = amount;
+                feet = amount / conversion;
+                Console.WriteLine(memes + " memes ");
+
+                Console.WriteLine("Would you like to do another conversion? ");
+                response = Console.ReadLine();
+
+            }
+            else 
+            {
+                Console.WriteLine("Good bye! ");
+            }
+        }
+
+        
+       
+
+    }
+}
